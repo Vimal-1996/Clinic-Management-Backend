5efe43c8ff613cd048bfa896dc36bcd44fece997
vimal-1963 <vmathew7@my.centennialcollege.ca>
2024-07-10 14:28:10 -0400

    modifications_1

--- Login_Func/adminLogin.js
+++ Login_Func/adminLogin.js
@@ -5,7 +5,7 @@ const Patient = require('../database/patientModel');
 const Doctor = require('../database/doctorModel');
 const Appointment = require('../database/appointmentModel')
 
-const { generateWebToken,verifyWebToken } = require('../jwt/jwtfunctions');
+const { generateWebToken, verifyWebToken } = require('../jwt/jwtfunctions');
 const { session } = require('passport');
 
 
@@ -14,7 +14,7 @@ router.get('/login', (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-   
+
     const filter = {
         email: req.body.email,
         password: req.body.password
@@ -23,7 +23,7 @@ router.post('/login', async (req, res) => {
         const admin = await Admin.findOne(filter, { _id: 1, email: 1, password: 1 })
         if (admin) {
             const payload = { userId: admin._id, email: admin.email }
-            await generateWebToken(payload).then((token) => { res.status(201).json({ message: "User found",token:token})}).catch((err) => {  res.status(500).json({ message: "User doesn't exist, Check your username and password" })})  
+            await generateWebToken(payload).then((token) => { res.status(201).json({ message: "User found", token: token }) }).catch((err) => { res.status(500).json({ message: "User doesn't exist, Check your username and password" }) })
         } else {
             res.status(500).json({ message: "User doesn't exist, Check your username and password" })
         }
@@ -34,120 +34,134 @@ router.post('/login', async (req, res) => {
 })
 
 
-router.get('/doctorDetails',async(req,res)=>{
-    try{  
-        const doctors =await Doctor.find({},{_id:1,doctorName:1,specialised:1,speciality:1,role:1,accountStatus:1})
-        if(doctors){
-            res.status(201).json({doctors:doctors})
-        }else{
+router.get('/doctorDetails', async (req, res) => {
+    try {
+        const doctors = await Doctor.find({}, { _id: 1, doctorName: 1, specialised: 1, speciality: 1, role: 1, accountStatus: 1 })
+        if (doctors) {
+            res.status(201).json({ doctors: doctors })
+        } else {
             res.status(500).json({ message: "Error fetching patient details" })
         }
-    }catch(err){
+    } catch (err) {
         res.status(401).json({ message: "failed api call" })
     }
 })
 
-router.get('/patientDetails',async(req,res)=>{
-    try{  
-        
-        const patients =await Patient.find({},{_id:1,patientName:1,age:1,mobileNumber:1,role:1,accountStatus:1})
-        if(patients){
-            res.status(201).json({patients:patients})
-        }else{
+router.get('/patientDetails', async (req, res) => {
+    try {
+
+        const patients = await Patient.find({}, { _id: 1, patientName: 1, age: 1, mobileNumber: 1, role: 1, accountStatus: 1 })
+        if (patients) {
+            res.status(201).json({ patients: patients })
+        } else {
             res.status(500).json({ message: "Error fetching patient details" })
         }
-    }catch(err){
+    } catch (err) {
         res.status(401).json({ message: "failed api call" })
     }
 })
 
 
-router.post('/addnewdoctor',async(req,res)=>{
-    const {doctorName,specialised,speciality} = req.body
-    try{
-        const doctor = new Doctor({doctorName,specialised,speciality,accountStatus:true, email:`${doctorName}@gmail.com`, password:"123456"})
+router.post('/addnewdoctor', async (req, res) => {
+    const { doctorName, specialised, speciality } = req.body
+    try {
+        const doctor = new Doctor({ doctorName, specialised, speciality, accountStatus: true, email: `${doctorName}@gmail.com`, password: "123456" })
         await doctor.save();
-        res.status(201).json({message:"New Doctor created"})
+        res.status(201).json({ message: "New Doctor created" })
     }
-    catch(err){
-        res.status(401).json({message:"Doctor creation failed"})
+    catch (err) {
+        res.status(401).json({ message: "Doctor creation failed" })
     }
-    
-    
+
+
 })
 
-router.post('/addnewpatient',async(req,res)=>{
-    const {patientName, patientAge, patientMobileNumber} = req.body
-    try{
-        const patient = new Patient({patientName,age:patientAge,mobileNumber:patientMobileNumber,accountStatus:true,email:`${patientName}@gmail.com`, password:`${patientName}`})
+router.post('/addnewpatient', async (req, res) => {
+    const { patientName, patientAge, patientMobileNumber } = req.body
+    try {
+        const patient = new Patient({ patientName, age: patientAge, mobileNumber: patientMobileNumber, accountStatus: true, email: `${patientName}@gmail.com`, password: `${patientName}` })
         await patient.save();
-        res.status(201).json({message:"New Patient created"})
+        res.status(201).json({ message: "New Patient created" })
     }
-    catch(err){
-        res.status(401).json({message:"Patient creation failed"})
+    catch (err) {
+        res.status(401).json({ message: "Patient creation failed" })
     }
 })
 
-router.post('/addnewappointment',async(req,res)=>{
-    
-    
-    const {appointmentDate,
+router.post('/addnewappointment', async (req, res) => {
+
+
+    const { appointmentDate,
         appointmentTime,
         session,
         doctorName,
-        patientName} = req.body
+        patientName } = req.body
 
-    function createRefId(){
+    function createRefId() {
         const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
         let result = '';
         for (let i = 0; i < 6; i++) {
-          result += characters.charAt(Math.floor(Math.random() * characters.length));
+            result += characters.charAt(Math.floor(Math.random() * characters.length));
         }
         return result;
-      }
-    
-
-    try{
-        const appointment = new Appointment({appointmentDate:appointmentDate,session:session,time:appointmentTime,doctorName:doctorName,patientName:patientName,appointmentRefid:createRefId(),appointmentVisibile:true,appointmentStatus:"Confirmed"})
-        await appointment.save();
-        res.status(201).json({message:"New Appointment created"})
     }
-    catch(err){
-        res.status(401).json({message:"Appointment creation failed"})
+
+
+    try {
+        const appointment = new Appointment({ appointmentDate: appointmentDate, session: session, time: appointmentTime, doctorName: doctorName, patientName: patientName, appointmentRefid: createRefId(), appointmentVisibile: true, appointmentStatus: "Confirmed" })
+        await appointment.save();
+        res.status(201).json({ message: "New Appointment created" })
+    }
+    catch (err) {
+        res.status(401).json({ message: "Appointment creation failed" })
     }
 })
 
-router.get('/allappointments',async(req,res)=>{
-    try{
+router.get('/allappointments', async (req, res) => {
+    try {
         const appointmentQueryResult = await Appointment.aggregate([
             {
                 $lookup: {
-                    from: "doctors",           
-                    localField: "doctorName",  
-                    foreignField: "_id",       
+                    from: "doctors",
+                    localField: "doctorName",
+                    foreignField: "_id",
                     as: "doctorDetails"
-                  }
-            },
-            {
-                $unwind:"$doctorDetails"
-            },
-            {
-                $lookup:{
-                    from:"patients",
-                    localField:"patientName",
-                    foreignField:"_id",
-                    as:"patientDetails"
                 }
             },
             {
-                $unwind:"$patientDetails"
+                $unwind: "$doctorDetails"
+            },
+            {
+                $lookup: {
+                    from: "patients",
+                    localField: "patientName",
+                    foreignField: "_id",
+                    as: "patientDetails"
+                }
+            },
+            {
+                $unwind: "$patientDetails"
             }
         ]);
-        res.status(200).json({"appointments":appointmentQueryResult})
-    }catch(err){
+        res.status(200).json({ "appointments": appointmentQueryResult })
+    } catch (err) {
         console.log(err)
-        res.status(401).json({"Error":err});
+        res.status(401).json({ "Error": err });
     }
+})
+
+router.post('/updateappointmentstatus', async (req, res) => {
+    var appointmentStatusVariable;
+    if (req.body.appointmentStatus === 'confirmed') {
+        appointmentStatusVariable = 'pending'
+    } else {
+        appointmentStatusVariable = 'confirmed'
+    }
+    await Appointment.findOneAndUpdate({ appointmentRefid: req.body.referenceId }, { appointmentStatus: appointmentStatusVariable })
+        .catch((error) => {
+            console.log("Error during update")
+        })
+    res.status(201).json({ message: "updated appointment status" })
 })
 
 
